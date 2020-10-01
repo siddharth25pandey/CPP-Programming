6b7cdeb7348498c2757e0b77cb734cc0613cf51f
FawadMirza32 <37993847+FawadMirza32@users.noreply.github.com>
2020-10-01 20:08:43 +0530

    Create Sudoku_solver.cpp

---
+++ Backtrack/Sudoku_solver.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool found = false;
+void print(int cell[9][9])
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cout << cell[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+bool valid(int cell[9][9], int r, int c)
+{
+    //it is valid if same value is not stored in row or col or mini box
+    int br = r / 3;
+    int bc = c / 3;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (cell[r][c] == cell[3 * br + i][3 * bc + j] && (3 * br + i) != r && (3 * bc + j) != c)
+            {
+                return false;
+            }
+        }
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        if (cell[r][c] == cell[i][c] && i != r)
+        {
+            return false;
+        }
+        if (cell[r][c] == cell[r][i] && i != c)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool solved(int cell[9][9])
+{
+    for (int row = 0; row < 9; row++)
+    {
+        for (int col = 0; col < 9; col++)
+        {
+            //for each cell check 3 conditions
+            //1. This cell value should not repeat in entire row
+            //2. This cell value should not repeat in entire col
+            //3. This cell value should not repeat in entire 3*3 mini box
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    if (!valid(cell, r, c))
+                        return false;
+                }
+            }
+            // int val = cell[row][col];
+            // for (int k = 0; k < 9; k++)
+            // {
+            //     if (val == cell[row][k] && k != col)
+            //         return false;
+            //     if (val == cell[k][col] && k != row)
+            //         return false;
+            // }
+        }
+    }
+    return true;
+}
+void backtrack(int cell[9][9], int &k)
+{
+    if (found)
+        return;
+    //if we are at search space (or this is leaf node)
+    if (k == 81)
+    {
+        //print(cell);
+        cout << endl;
+        if (solved(cell))
+        {
+            cout << "Solution of sudoku puzzle is >> " << endl;
+            print(cell);
+            found = true;
+        }
+        return;
+    }
+    //cout << k << endl;
+    //print(cell);
+    int c = k % 9;
+    int r = k / 9;
+    //cout << r << " " << c << endl;
+    k++;
+    for (int val = 1; val <= 9; val++)
+    {
+        if (cell[r][c] != 0)
+        {
+            if (valid(cell, r, c))
+            {
+                backtrack(cell, k);
+                break;
+            }
+            else
+                return;
+        }
+        cell[r][c] = val;
+        if (valid(cell, r, c))
+            backtrack(cell, k);
+        cell[r][c] = 0;
+    }
+    k--;
+}
+
+int main()
+{
+    //int cell[9][9];
+    int cell[9][9] = {
+        {0, 2, 0, 0, 0, 4, 3, 0, 0},
+        {9, 0, 0, 0, 2, 0, 0, 0, 8},
+        {0, 0, 0, 6, 0, 9, 0, 5, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {0, 7, 2, 5, 0, 3, 6, 8, 0},
+        {6, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 8, 0, 2, 0, 5, 0, 0, 0},
+        {1, 0, 0, 0, 9, 0, 0, 0, 3},
+        {0, 0, 9, 8, 0, 0, 0, 6, 0}};
+    // int cell[9][9] = {
+    //     {0, 3, 0, 0, 9, 0, 6, 1, 0},
+    //     {6, 0, 8, 5, 0, 3, 4, 9, 7},
+    //     {0, 9, 0, 6, 7, 0, 0, 0, 3},
+    //     {0, 5, 0, 8, 0, 4, 0, 0, 1},
+    //     {1, 6, 0, 3, 0, 0, 9, 8, 2},
+    //     {0, 0, 2, 9, 6, 0, 3, 0, 0},
+    //     {0, 8, 0, 1, 3, 0, 2, 0, 6},
+    //     {3, 0, 5, 0, 4, 6, 0, 7, 9},
+    //     {0, 4, 6, 0, 8, 0, 1, 0, 0}};
+    print(cell);
+    int k = 0;
+    backtrack(cell, k);
+}
