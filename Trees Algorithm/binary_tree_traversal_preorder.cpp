f4781e6ce8f0f8a1b8cd2a8b417f7657162ce3d9
shubhangi-singh21 <shubhangifas@gmail.com>
2020-10-02 14:54:37 +0530

    added code in preorder traversal

--- Trees Algorithm/binary_tree_traversal_preorder.cpp
+++ Trees Algorithm/binary_tree_traversal_preorder.cpp
@@ -1,21 +1,41 @@
 //Preorder-> root, left, right
 
 
-#include<bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h> 
+using namespace std; 
 
-void Preorder( struct node *root)
+struct Node 
+{ 
+    int data; 
+    struct Node* left; 
+    struct Node* right; 
+    Node (int data) 
+    { 
+        this->data = data; 
+        left = right = NULL; 
+    } 
+}; 
+  
+void Preorder( struct Node *root)
 {
   if(root==0)
   { return 0;}
  printf("%d", root->data);
- Preorder( root->left);
- Preorder( root->right);
+ Inorder( root->left);
+ Inorder( root->right);
 }
+  
 
-void main()
-{
-  struct node *root;
-  printf( "Preorder is:");
-  Preorder(root);
-}
+int main() 
+{ 
+  
+    struct Node *root = new Node(1); 
+    root->left        = new Node(2); 
+    root->right       = new Node(3); 
+    root->left->left  = new Node(4); 
+    root->left->right = new Node(5); 
+  
+    Preorder(root); 
+    return 0; 
+} 
+
