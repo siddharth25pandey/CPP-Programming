bbce2a849fc9eecf4dd88f9d8d6a3ae6b6bda354
brijeshvermav10 <72918867+brijeshvermav10@users.noreply.github.com>
2020-10-15 16:11:02 +0530

    Created Missing_number.cpp

---
+++ Randomized Algorithms/Missing_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  int n, x;
+  cin >> n;
+  ll sum = 0;
+  for (int i = 0; i < n - 1; i++) {
+    cin >> x;
+    sum += x;
+  }
+  cout << ll(n) * (n + 1) / 2 - sum << endl;
+}
