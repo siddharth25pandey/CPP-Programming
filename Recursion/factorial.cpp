d20f2cddbb9d66573e207ea6710a6548ecf335ac
neil95 <anuragchatterjee4@gmail.com>
2021-10-14 21:36:39 +0530

    Anurag Chatterjee|factorial.cpp|added factorial using recursion c++ code

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,23 @@
+// find factorial of a number c++ code
+#include<iostream>
+using namespace std;
+
+// recursive function used  to find the factorial
+int findFactorial(int num){
+    // base condition
+    if(num==0 || num==1){
+        return 1;
+    }
+    // here function is calling itself recursively till until it hits the base condition
+    // and multiplying the num with its previous function call returned value
+    return num * findFactorial(num-1);
+}
+
+int main(){
+    int number;
+    cin>>number;
+    
+    // calling function and displaying the result
+    cout<<"The factorial of "<<number<<" is "<<findFactorial(number);
+    return 0;
+}
