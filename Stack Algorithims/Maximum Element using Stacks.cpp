97b45a66ce5c9598ce42f5e4750e3f9d5a96e1ed
avijeet108 <kumaravijeet108@gamil.com>
2021-10-15 00:22:12 +0530

    Added Maximum Elements using Stacks in Stack Section

---
+++ Stack Algorithims/Maximum Element using Stacks.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() 
+{
+    stack<int> s1,s2;
+    int n,t,val;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        if(n==1)
+        { 
+            cin>>val;
+            s1.push(val);
+            if(s2.empty())
+            s2.push(val);
+            else 
+            {
+                if(val>s2.top())
+                s2.push(val);
+                else 
+                s2.push(s2.top());
+            }
+        }
+        else if(n==2)
+        {
+            s1.pop();
+            s2.pop();
+        }
+        else if(n==3)
+        {
+            cout<<s2.top()<<endl;
+        }
+    } 
+    return 0;
+}
