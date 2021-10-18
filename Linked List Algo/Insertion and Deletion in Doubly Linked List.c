3726c6d02981d8a44d7a49e214db3fee0c7d6963
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2021-10-18 14:41:04 +0530

    Changed the code from c to cpp

--- Linked List Algo/Insertion and Deletion in Doubly Linked List.c
+++ Linked List Algo/Insertion and Deletion in Doubly Linked List.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 // structure of Doubly Linked List (DLL)
 typedef struct node
@@ -15,48 +15,37 @@ DLL *head;
 void insert(int val)
 {
     // Allocating space and storing val top new Node
-    DLL *newNode = (DLL*)malloc(sizeof(DLL*));
+    DLL *newNode = (DLL *)malloc(sizeof(DLL *));
     newNode->data = val;
 
-    // first node case
-    if(head == NULL)
-    {
-        newNode->next = NULL;
-        newNode->prev = NULL;
-        head = newNode;
-    }
-    // inserting in the beginning
-    else
-    {
-        newNode->prev = NULL;
-        newNode->next = head;
-        
-        // creating Link between earlier first node and new node (new first node)
-        head->prev = newNode;
-        
-        // making newNode as head node
-        head = newNode;
-    }
+    // Adding node in the beginning
+    newNode->next = head;
+
+    // creating backward link
+    if(head != NULL) head->prev = newNode;
+
+    // making new node as head node
+    head = newNode;
 }
 
 void deleteNode(int val)
 {
     DLL *temp = head;
     // iterating through the List
-    while(temp != NULL)
+    while (temp != NULL)
     {
         // Finding the element
-        if(temp->data == val)
+        if (temp->data == val)
         {
             // only one node is present
-            if(temp->prev == NULL && temp->next == NULL)
+            if (temp->prev == NULL && temp->next == NULL)
             {
                 head = NULL;
                 free(temp);
                 return;
             }
             // node is present in middle of the List
-            if(temp->prev != NULL && temp->next != NULL)
+            if (temp->prev != NULL && temp->next != NULL)
             {
                 DLL *rear = temp->prev;
                 DLL *forward = temp->next;
@@ -66,7 +55,7 @@ void deleteNode(int val)
                 return;
             }
             // First node
-            if(temp->prev == NULL)
+            if (temp->prev == NULL)
             {
                 head = temp->next;
                 head->prev = NULL;
@@ -74,7 +63,7 @@ void deleteNode(int val)
                 return;
             }
             // Last node
-            if(temp->next == NULL)
+            if (temp->next == NULL)
             {
                 temp->prev->next = NULL;
                 free(temp);
@@ -88,15 +77,15 @@ void deleteNode(int val)
 // printing List
 void printList()
 {
-    DLL *temp = head;
-    while(temp->next != NULL)
+    DLL *ptr;
+    ptr = head;
+    while (ptr->next != NULL)
     {
-        printf("%d->",temp->data);
-        temp = temp->next;
+        cout << ptr->data << "->";
+        ptr = ptr->next;
     }
-    printf("%d\n",temp->data);
+    cout << ptr->data << "\n";
 }
-
 
 int main()
 {
@@ -112,22 +101,22 @@ int main()
     insert(2);
     insert(1);
 
-    printf("Original List\n");
+    cout << "Original List\n";
     printList();
 
     // case when node to be deleted is in the middle
     deleteNode(5);
-    printf("\nList after deleting 5\n");
+    cout << "\nList after deleting 5\n";
     printList();
 
     // case when first node is to be deleted
     deleteNode(1);
-    printf("\nList after deleting 1\n");
+    cout << "\nList after deleting 1\n";
     printList();
-    
+
     // case when last ndoe is to be deleted
     deleteNode(10);
-    printf("\nList after deleting 10\n");
+    cout << "\nList after deleting 10\n";
     printList();
     return 0;
 }
