ac5ef3a009b774a2e1bcc16d26150e99b17f44be
Anmol Mahajan <a.mahajan110104@gmail.com>
2022-10-05 20:07:24 +0530

    Added descriptive comments

--- DP/tribonnaciUsingDp.cpp
+++ DP/tribonnaciUsingDp.cpp
@@ -7,8 +7,10 @@ const int N = 1000009;
 int dp[N];
     
 int func(int n){
+    //checking if the ith element is not -1
     if(dp[n]!=-1)
         return dp[n];
+    //returning 0 for n=0 , 1 for n=1 and n=2.    
     if(n<2)
         return n;
     if(n==2)
@@ -19,6 +21,7 @@ int func(int n){
 }
     
 int tribonacci(int n) {
+       //assigning -1 to all the elements of dp array 
        memset(dp, -1, sizeof(dp));
        int ans = func(n);
        return ans; 
