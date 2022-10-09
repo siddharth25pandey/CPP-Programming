a86e5be369ccd3f7ed9ece00c89328293cbbc446
Manmeet Singh <85981620+ManmeetSingh1012@users.noreply.github.com>
2022-10-09 18:45:58 +0530

    C++ Patterns

---
+++ C++ Pattern problem/thirdpattren.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   for(int i=1;i<=5;i++)
+   {
+      for(int j=1;j<=i;j++)
+      {
+         cout<<i;
+      }
+      cout<<"\n";
+
+   }
+   return 0;
+}
+
+/**
+   1
+   22
+   333
+   4444
+   55555
+ * 
+ */
+ 
